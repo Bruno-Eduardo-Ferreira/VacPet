aa02836c1873bba30a7fc71447e23eff73071766
Bruno Eduardo Ferreira Cordeiro <102911080+Bruno-Eduardo-Ferreira@users.noreply.github.com>
2022-06-19 00:25:21 -0300

    exclusão de clientes e ajuste de cor do ícone do wpp

--- lib/ui/pages/consultas/consulta_cliente/consulta_cliente_page.dart
+++ lib/ui/pages/consultas/consulta_cliente/consulta_cliente_page.dart
@@ -15,6 +15,10 @@ class _ConsultaClienteState extends State<ConsultaCliente> {
     return _firestore.collection('clientes').snapshots();
   }
 
+  Future _deleteUser(String idUser) async {
+    var collection = FirebaseFirestore.instance.collection('clientes').doc(idUser).delete();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,28 +72,107 @@ class _ConsultaClienteState extends State<ConsultaCliente> {
                         return Padding(
                           padding: const EdgeInsets.fromLTRB(28, 12, 28, 12),
                           child: Container(
-                            padding: const EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                  width: 0.5, color: Colors.blue.shade200),
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(6.0)),
-                              color: Colors.white,
-                            ),
-                            child: ListTile(
-                              title: Text(
-                                doc['nome'],
-                                style: const TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.w600),
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 0.5, color: Colors.blue.shade200),
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(6.0)),
+                                color: Colors.white,
                               ),
-                            ),
-                          ),
+                              child: cardCliente(doc.id, doc['nome'], doc['celular']),),
                         );
                       });
               }
             }),
       )),
+    );
+  }
+
+  Widget cardCliente(String idUser, String nome, String celular) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () async {},
+          child: ListTile(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  nome,
+                  style: const TextStyle(
+                      fontSize: 22.0, fontWeight: FontWeight.w600),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Center(
+                              child: Text(
+                                'Atenção!',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w600,
+                                    color: Colors.red),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: Icon(
+                                Icons.warning_amber_outlined,
+                                color: Colors.red,
+                                size: 40,
+                              ),
+                            )
+                          ],
+                        ),
+                        content: const Text(
+                          'Você tem certeza que deseja excluir permanentemente o cadastro desse cliente?',
+                          style: TextStyle(
+                              fontSize: 15, fontWeight: FontWeight.w400),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text(
+                              'Não',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              _deleteUser(idUser);
+                              Navigator.pop(context, 'OK');
+                            },
+                            child: const Text(
+                              'Sim',
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  child: const Icon(
+                    Icons.delete_rounded,
+                    color: Colors.red,
+                    size: 35,
+                  ),
+                )
+              ],
+            ),
+            subtitle: Text('Celular: $celular',
+                style: const TextStyle(
+                    fontSize: 15.0, height: 2, fontWeight: FontWeight.w600),),
+          ),
+        ),
+      ],
     );
   }
 }
